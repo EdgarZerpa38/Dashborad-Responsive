3a8db85e627259cb67fe9ee645fcb4c010d534d3
Edgar Zerpa <edgarzerpa38@gmail.com>
2018-07-09 17:10:37 -0400

    Primer commit

---
+++ kunder/js/main.js
@@ -0,0 +1,12 @@
+/*toggle1 */
+$("#tooglea").click(function () {
+    $("#submenuA").toggle("slow");
+});
+/*toggle2 */
+$("#toogleb").click(function () {
+    $("#submenuB").toggle("slow");
+});
+$("#tooglera").click(function () {
+    $("#submenuRA").toggle("slow");
+});
+
